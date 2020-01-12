b6bb2d3042645af56fd4d320e98a3dc2a622df91
yanzhen74 <yanzhen@csu.ac.cn>
2020-01-12 21:15:25 +0800

    feature: add parameter table to selfdefine
    1. add js
    2. add route
    
    null

--- src/routes/usertree_route.go
+++ src/routes/usertree_route.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/hero"
+	"github.com/yanzhen74/goview/src/controller"
 	"github.com/yanzhen74/goview/src/middleware/jwts"
+	"github.com/yanzhen74/goview/src/supports"
 )
 
 func UserTreeHub(party iris.Party) {
@@ -13,6 +15,7 @@ func UserTreeHub(party iris.Party) {
 	usertree.Get("/add", hero.Handler(UserTreeAdd))
 	usertree.Get("/edit", hero.Handler(UserTreeEdit))
 	usertree.Get("/del", hero.Handler(UserTreeDel))
+	usertree.Post("/keysearch", hero.Handler(UserTreeKeySearch))
 }
 
 func UserTreeAdd(ctx iris.Context) {
@@ -31,5 +34,24 @@ func UserTreeEdit(ctx iris.Context) {
 }
 
 func UserTreeDel(ctx iris.Context) {
+
+}
+
+func UserTreeKeySearch(ctx iris.Context) {
+	type key_search struct {
+		Key string `json:"key_search"`
+	}
+
+	var (
+		err error
+		key = new(key_search)
+	)
+
+	if err = ctx.ReadJSON(&key); err != nil {
+		ctx.Application().Logger().Errorf("search [%s] not valid. %s", key.Key, err.Error())
+		supports.Error(ctx, iris.StatusBadRequest, "not valid", nil)
+	}
+
+	supports.Ok(ctx, "ok", controller.Dicts)
 
 }
